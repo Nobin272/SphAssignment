ff726dcf80dd866520b3fae15a6c99795d20c579
Nobin Thomas <nobin272@yahoo.com>
2019-01-22 19:38:01 +0800

    Initial Commit

---
+++ SphAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SphAssignment
+//
+//  Created by Nobin Thomas on 22/1/19.
+//  Copyright © 2019 Nobin Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
